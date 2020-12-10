2fa12164ea2bcc76ec5bea6a586f9851720fb4e5
Kushal M <53528619+Kushal148@users.noreply.github.com>
2020-12-10 12:05:41 +0530

    Map api.

---
+++ Googleapipoc/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Googleapipoc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Googleapipoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(Coords co)
+        {
+            Coords s = new Coords { lattitude = co.lattitude, longitude = co.longitude };
+            return View(s);
+        }
+     
+    }
+}
---
+++ Googleapipoc/Models/Coords.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Googleapipoc.Models
+{
+    public class Coords
+    {
+        public Double longitude { get; set; }
+        public Double lattitude { get; set; }
+    }
+}
